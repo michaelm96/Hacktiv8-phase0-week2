763a7cd55238e3264713898b58cbb10d0f93af28
Michael M <47624034+michaelm96@users.noreply.github.com>
2020-02-28 21:52:15 +0700

    Update String.js

--- tugas minggu 2/String/String.js
+++ tugas minggu 2/String/String.js
@@ -61,4 +61,4 @@ console.log('First Word: ' + exampleFirstWord4 + ', with length: ' + firstWordLe
 console.log('Second Word: ' + secondWord4 + ', with length: ' + secondWordLength);
 console.log('Third Word: ' + thirdWord4 + ', with length: ' + thirdWordLength);
 console.log('Fourth Word: ' + fourthWord4 + ', with length: ' + fourthWordLength);
-console.log('Fifth Word: ' + fifthWord4 + ', with length: ' + secondWordLength);
+console.log('Fifth Word: ' + fifthWord4 + ', with length: ' + fifthWordLength);

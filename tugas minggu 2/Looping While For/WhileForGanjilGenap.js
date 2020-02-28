8f1671085247e115ff9881a5c3eb0f1081fc1c95
Michael M <47624034+michaelm96@users.noreply.github.com>
2020-02-28 16:19:55 +0700

    Update and rename WhileFor.js to WhileForGanjilGenap.js

--- tugas minggu 2/Looping While For/WhileForGanjilGenap.js
+++ tugas minggu 2/Looping While For/WhileForGanjilGenap.js
@@ -23,3 +23,40 @@ console.log('lOOPING KEDUA');
 for (var l = 20; l >= 1; l--) {
 	console.log(l + ' - I will become fullstack developer');
 }
+
+// Ganjil Genap
+
+for (var i = 0; i <= 100; i++) {
+	if (i % 2 == 0) {
+		console.log('Genap');
+	} else if (i % 2 !== 0) {
+		console.log('Ganjil');
+	}
+}
+console.log("");
+
+for (var j = 1; j <= 100; j+=2){
+	if(j % 3 == 0){
+		console.log(j + " kelipatan 3");
+	}else{
+		
+	}
+}
+
+console.log("");
+for (var k = 1; k <= 100; k+=5){
+	if(k % 6 == 0){
+		console.log(k + " kelipatan 6");
+	}else{
+		
+	}
+}
+
+console.log("");
+for (var l = 1; l <= 100; l+=9){
+	if(l % 10 == 0){
+		console.log(l + " kelipatan 10");
+	}else{
+		
+	}
+}
